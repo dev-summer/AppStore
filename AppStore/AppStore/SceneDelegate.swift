9c3424ce4640b4dfdddb0b03b2a43d627da952e9
summercat <summerly812@gmail.com>
2023-07-02 15:27:34 +0900

    feat: 프로젝트 생성 및 코드베이스 UI 세팅

---
+++ AppStore/AppStore/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/02.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let viewController = ViewController()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
