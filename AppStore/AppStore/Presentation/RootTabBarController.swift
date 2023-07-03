43e9621880093d9131f1d0cf493b1b3e18c21072
summercat <summerly812@gmail.com>
2023-07-03 18:57:58 +0900

    feat: RootTabBar에 TodayVC 탭 추가

--- AppStore/AppStore/Presentation/RootTabBarController.swift
+++ AppStore/AppStore/Presentation/RootTabBarController.swift
@@ -8,7 +8,37 @@
 import UIKit
 
 final class RootTabBarController: UITabBarController {
+    private enum Constant {
+        enum Tabs {
+            static let firstTabName: String = "Today"
+            static let firstTabIcon: String = "doc.text.image"
+        }
+    }
+    
+    private var tabs: [UIViewController]? = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTabs()
+    }
+    
+    private func configureTabs() {
+        createNewTab(
+            rootViewController: TodayViewController(),
+            tabName: Constant.Tabs.firstTabName,
+            iconName: Constant.Tabs.firstTabIcon
+        )
+        viewControllers = tabs
+    }
+    
+    private func createNewTab(
+        rootViewController: UIViewController,
+        tabName title: String,
+        iconName: String
+    ) {
+        let tab = UINavigationController(rootViewController: rootViewController)
+        tab.tabBarItem.title = title
+        tab.tabBarItem.image  = UIImage(systemName: iconName)
+        tabs?.append(tab)
     }
 }
---
+++ AppStore/AppStore/Presentation/Today/TodayViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TodayViewController.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/03.
+//
+
+import UIKit
+
+final class TodayViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
