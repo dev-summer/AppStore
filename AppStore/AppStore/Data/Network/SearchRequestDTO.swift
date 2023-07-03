29d79ba5284c76e6d3abc917ac9c945c008431c0
summercat <summerly812@gmail.com>
2023-07-03 18:34:44 +0900

    feat: SearchRequestDTO 타입 구현

---
+++ AppStore/AppStore/Data/Network/SearchRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  SearchRequestDTO.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/03.
+//
+
+struct SearchRequestDTO {
+    let term: String
+    let offset: Int
+    let limit: Int
+}
