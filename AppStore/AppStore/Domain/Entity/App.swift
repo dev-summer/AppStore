47cd253a46e5750274a38c670e821aac6cc559a5
summercat <summerly812@gmail.com>
2023-07-03 19:22:45 +0900

    feat: App, AppsPage 엔티티 구현

---
+++ AppStore/AppStore/Domain/Entity/App.swift
@@ -0,0 +1,27 @@
+//
+//  App.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/03.
+//
+
+struct App {
+    let appID: Int
+    let appName: String
+    let price: Double
+    let formattedPrice: String
+    let categoryName: String
+    let appIconURL: String
+    let averageUserRating: Double
+    let userRatingCount: Int
+    let screenshotURLs: [String]
+    let contentAdvisoryRating: String
+    let languageCodesISO2A: [String]
+    let fileSizeBytes: String
+    let description: String
+    let releaseNotes: String?
+    let version: String
+    let categories: [String]
+    let providerName: String
+    let minimumOSVersion: String
+}
