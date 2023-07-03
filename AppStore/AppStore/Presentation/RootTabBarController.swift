38ddbda6788220257659df7a523e07a3a1e269c7
summercat <summerly812@gmail.com>
2023-07-03 18:53:03 +0900

    feat: RootTabBarVC를 window의 기본 VC로 설정

---
+++ AppStore/AppStore/Presentation/RootTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  RootTabBarController.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/03.
+//
+
+import UIKit
+
+final class RootTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
