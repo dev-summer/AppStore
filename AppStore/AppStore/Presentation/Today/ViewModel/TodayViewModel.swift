3ee44ec69e25e117ea8acefadc4fa6dd36df6b6d
summercat <summerly812@gmail.com>
2023-07-08 19:20:17 +0900

    feat: TodayViewModel 구현

---
+++ AppStore/AppStore/Presentation/Today/ViewModel/TodayViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  TodayViewModel.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/03.
+//
+
+final class TodayViewModel {
+    private enum Query {
+        static let exampleID: Int = 544007664
+        static let exampleKeyword: String = "캘린더"
+        static let page: Int = .zero
+        static let pageSize: Int = 5
+    }
+    
+    var appsDelivered: (([Section: [TodayItem]]) -> Void)?
+    private let useCase: SearchAppUseCase
+    private var apps: [Section: [TodayItem]] = [:] {
+        didSet {
+            appsDelivered?(apps)
+        }
+    }
+    
+    init(useCase: SearchAppUseCase = DefaultSearchAppUseCase()) {
+        self.useCase = useCase
+        Section.allCases.forEach { apps[$0] = [] }
+    }
+    
+    func fetchApps() {
+        searchAppInfo(with: Query.exampleID)
+        searchAppList(with: Query.exampleKeyword, page: Query.page, pageSize: Query.pageSize)
+    }
+    
+    private func searchAppInfo(with id: Int) {
+        useCase.searchApp(with: id) { [weak self] result in
+            switch result {
+            case .success(let app):
+                let item = TodayItem(app: app, type: .large)
+                self?.apps[item.type]?.append(item)
+            case .failure(let error):
+                // TODO: 에러처리
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func searchAppList(with keyword: String, page: Int, pageSize: Int) {
+        useCase.searchAppList(with: keyword, page: page, pageSize: pageSize) { [weak self] result in
+            switch result {
+            case .success(let appsPage):
+                let items: [TodayItem] = appsPage.searchResults.compactMap { TodayItem(app: $0, type: .list) }
+                self?.apps[.list]?.append(contentsOf: items)
+            case .failure(let error):
+                // TODO: 에러처리
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
