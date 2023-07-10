b784c89f57ac992594cf2042dc7973f0e640a235
summercat <summerly812@gmail.com>
2023-07-10 23:59:35 +0900

    feat: AppListViewModel 구현
    - list셀을 탭하면 앱 목록을 보여주는 뷰의 뷰모델

---
+++ AppStore/AppStore/Presentation/Today/AppList/ViewModel/AppListViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  AppListViewModel.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/10.
+//
+
+final class AppListViewModel {
+    private enum Query {
+        static let page: Int = .zero
+        static let pageSize: Int = 20
+    }
+    
+    var description: String?
+    var title: String?
+    var appsDelivered: (([TodayItem]) -> Void)?
+    private let useCase: SearchAppUseCase
+    private let keyword: String
+    
+    init(useCase: SearchAppUseCase = DefaultSearchAppUseCase(), keyword: String) {
+        self.useCase = useCase
+        self.keyword = keyword
+    }
+    
+    func fetchAppList() {
+        useCase.searchAppList(with: keyword, page: Query.page, pageSize: Query.pageSize) { [weak self] result in
+            switch result {
+            case .success(let appsPage):
+                let items: [TodayItem] = appsPage.searchResults.compactMap { TodayItem(app: $0, type: .list) }
+                self?.appsDelivered?(items)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
