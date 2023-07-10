dd2e3bd74c43d77234a2d98252dad1a4e58b2846
summercat <summerly812@gmail.com>
2023-07-11 00:00:44 +0900

    feat: AppListViewController 구현
    - list셀을 탭하면 앱 리스트를 보여주는 뷰 컨트롤러

---
+++ AppStore/AppStore/Presentation/Today/AppList/View/AppListViewController.swift
@@ -0,0 +1,174 @@
+//
+//  AppListViewController.swift
+//  AppStore
+//
+//  Created by summercat on 2023/07/10.
+//
+
+import UIKit
+
+final class AppListViewController: UIViewController {
+    private enum Namespace {
+        static let closeButtonIcon: String = "xmark.circle.fill"
+        static let buttonBackground: String = "circle.fill"
+    }
+    
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, TodayItem>
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, TodayItem>
+    
+    private let viewModel: AppListViewModel
+    private var dataSource: DataSource?
+    
+    private var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+        collectionView.backgroundColor = .systemBackground
+        return collectionView
+    }()
+    
+    private var closeButton: UIButton = {
+        let button = UIButton()
+        let iconConfig = UIImage.SymbolConfiguration(pointSize: 40)
+        let image = UIImage(systemName: Namespace.closeButtonIcon, withConfiguration: iconConfig)?
+            .withRenderingMode(.alwaysTemplate)
+        let background = UIImage(systemName: Namespace.buttonBackground)?
+            .withTintColor(.systemBackground, renderingMode: .alwaysTemplate)
+        button.setImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        button.imageView?.tintColor = .secondarySystemBackground
+        button.setBackgroundImage(background, for: .normal)
+        return button
+    }()
+    
+    init(viewModel: AppListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierarchy()
+        configureConstraints()
+        configureCollectionView()
+        configureCloseButtonAction()
+        bind()
+        viewModel.fetchAppList()
+    }
+    
+    private func configureHierarchy() {
+        [collectionView, closeButton].forEach { view.addSubview($0) }
+    }
+    
+    private func configureConstraints() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            closeButton.widthAnchor.constraint(equalToConstant: 40),
+            closeButton.heightAnchor.constraint(equalTo: closeButton.widthAnchor)
+        ])
+    }
+    
+    private func configureCollectionView() {
+        collectionView.collectionViewLayout = createCollectionViewLayout()
+        configureDataSource()
+        registerSupplementaryViews()
+        configureSupplementaryViews()
+    }
+    
+    private func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { _, environment in
+            let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+            let header = self.createSectionHeaderItem()
+            let section = NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: environment)
+            section.boundarySupplementaryItems = [header]
+            
+            return section
+        }
+        
+        return layout
+    }
+    
+    private func createSectionHeaderItem() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(60)
+        )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .topLeading
+        )
+        
+        return header
+    }
+    
+    private func registerSupplementaryViews() {
+        collectionView.register(
+            SectionHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: SectionHeaderView.identifier
+        )
+    }
+    
+    private func configureSupplementaryViews() {
+        dataSource?.supplementaryViewProvider = { [weak self] _, _, indexPath in
+            return self?.createSectionHeaderView(indexPath)
+        }
+    }
+    
+    private func createSectionHeaderView(_ indexPath: IndexPath) -> SectionHeaderView? {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: SectionHeaderView.identifier,
+            for: indexPath
+        ) as? SectionHeaderView else { return nil }
+        
+        header.setTextFor(description: viewModel.description, title: viewModel.title)
+        
+        return header
+    }
+    
+    private func configureDataSource() {
+        let listCellRegistration = UICollectionView.CellRegistration<TodayListCell, TodayItem> { cell, _, item in
+          cell.bind(with: item)
+        }
+        
+        dataSource = DataSource(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, item in
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: listCellRegistration,
+                    for: indexPath,
+                    item: item
+                )
+            })
+    }
+    
+    private func configureCloseButtonAction() {
+        let action = UIAction { _ in
+            self.dismiss(animated: true)
+        }
+        closeButton.addAction(action, for: .allTouchEvents)
+    }
+    
+    private func bind() {
+        viewModel.appsDelivered = { [weak self] items in
+            var snapshot = Snapshot()
+            snapshot.appendSections([.list])
+            snapshot.appendItems(items, toSection: .list)
+            self?.dataSource?.apply(snapshot)
+        }
+    }
+}
