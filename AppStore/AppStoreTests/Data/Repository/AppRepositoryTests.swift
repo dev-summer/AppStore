f4fba6e403eb1206de2a67dbfa90d1bd176a2f58
summercat <summerly812@gmail.com>
2023-09-14 19:12:58 +0900

    test: AppResponse 타입의 프로퍼티 변경에 따른 테스트 수정

--- AppStore/AppStoreTests/Data/Repository/AppRepositoryTests.swift
+++ AppStore/AppStoreTests/Data/Repository/AppRepositoryTests.swift
@@ -12,12 +12,12 @@ final class AppRepositoryTests: XCTestCase {
     private let appResponse: AppResponse = AppResponse(
         appID: Int.zero,
         appName: String.init(),
+        appIconURL: String.init(),
+        appCategory: String.init(),
         price: Double.zero,
         formattedPrice: String.init(),
-        categoryName: .init(),
-        appIconURL: String.init(),
+        userRatingCount: Double.zero,
         averageUserRating: Double.zero,
-        userRatingCount: Int.zero,
         screenshotURLs: Array.init(),
         contentAdvisoryRating: String.init(),
         languageCodesISO2A: Array.init(),
@@ -25,10 +25,11 @@ final class AppRepositoryTests: XCTestCase {
         description: String.init(),
         releaseNotes: nil,
         version: String.init(),
-        categories: Array.init(),
         providerName: String.init(),
+        developerName: String.init(),
         minimumOSVersion: String.init()
     )
+    
     private var sut: AppRepository!
     
     func test_retrieveAppInfoFromNetworkSuccess() {
